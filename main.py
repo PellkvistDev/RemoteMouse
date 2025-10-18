a7c488d9cb7d58584e61fae18ff1d54be91ac2ca
PellkvistDev <theo@hihihi.se>
2025-10-18 09:31:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/")
+def serve_index():
+    return FileResponse("index.html")
